a7cf8c3070d45068affdeb4304306af502281d5d
jcagan87 <jcagan87@172.16.227.131>
2016-01-28 14:47:00 -0500

    i wrote hello somewhere in this doc find it and you will have the power to limitless chocolate!!!

--- Robot Code 2016/src/org/usfirst/frc/team2262/robot/Robot.java
+++ Robot Code 2016/src/org/usfirst/frc/team2262/robot/Robot.java
@@ -10,7 +10,7 @@ import edu.wpi.first.wpilibj.CameraServer;
 import edu.wpi.first.wpilibj.Encoder;
 import edu.wpi.first.wpilibj.Ultrasonic;
 
-
+//HELLOOOO
 
 /**
  * The VM is configured to automatically run this class, and to call the
